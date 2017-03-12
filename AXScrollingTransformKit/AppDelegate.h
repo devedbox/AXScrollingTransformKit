da634ccdf4606e1b48401e15c207c979d9c9f062
devedbox <862099730@qq.com>
2017-03-12 16:32:43 +0800

    Initial Commit

---
+++ AXScrollingTransformKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AXScrollingTransformKit
+//
+//  Created by devedbox on 2017/3/12.
+//  Copyright © 2017年 jiangyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AXScrollingTransformKitTests/AXScrollingTransformKitTests.m
@@ -0,0 +1,39 @@
+//
+//  AXScrollingTransformKitTests.m
+//  AXScrollingTransformKitTests
+//
+//  Created by devedbox on 2017/3/12.
+//  Copyright © 2017年 jiangyou. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AXScrollingTransformKitTests : XCTestCase
+
+@end
+
+@implementation AXScrollingTransformKitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
