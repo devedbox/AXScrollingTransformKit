c7838c95e14261fac9896cdfcf6a3cc5667c6bd8
devedbox <devedbox@gmail.com>
2017-03-13 10:02:40 +0800

    Added scroll view on the storyboard.

---
+++ AXScrollingTransformKitFramework/AXScrollingTransformKitFramework.h
@@ -0,0 +1,19 @@
+//
+//  AXScrollingTransformKitFramework.h
+//  AXScrollingTransformKitFramework
+//
+//  Created by devedbox on 2017/3/13.
+//  Copyright © 2017年 jiangyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AXScrollingTransformKitFramework.
+FOUNDATION_EXPORT double AXScrollingTransformKitFrameworkVersionNumber;
+
+//! Project version string for AXScrollingTransformKitFramework.
+FOUNDATION_EXPORT const unsigned char AXScrollingTransformKitFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AXScrollingTransformKitFramework/PublicHeader.h>
+
+
